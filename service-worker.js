67f8dc9441fb274c78250c158e8314fc579734cc
mancorago-web <mancora.go@gmail.com>
2026-04-16 18:59:26 -0500

    Fix merge conflicts and add all images

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 const CACHE_NAME = "mancora-go-v28";
-=======
-const CACHE_NAME = "mancora-go-v27";
->>>>>>> adeb23de7f1770426708d9ad40fac22647b74b4e
 
-// Delete ALL old caches on activate
 self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(cacheNames => {
@@ -25,10 +20,6 @@ self.addEventListener("fetch", event => {
   event.respondWith(
     fetch(event.request)
       .then(response => {
-<<<<<<< HEAD
-=======
-        // Clone and cache new responses
->>>>>>> adeb23de7f1770426708d9ad40fac22647b74b4e
         const responseClone = response.clone();
         caches.open(CACHE_NAME).then(cache => {
           cache.put(event.request, responseClone);
@@ -36,15 +27,7 @@ self.addEventListener("fetch", event => {
         return response;
       })
       .catch(() => {
-<<<<<<< HEAD
         return caches.match(event.request);
       })
   );
 });
-=======
-        // Fallback to cache
-        return caches.match(event.request);
-      })
-  );
-});
->>>>>>> adeb23de7f1770426708d9ad40fac22647b74b4e
