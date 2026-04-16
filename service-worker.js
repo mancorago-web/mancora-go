a45a5be8cf68571e7c0664f13ae4f427d14eaf93
mancorago-web <mancora.go@gmail.com>
2026-04-16 18:50:19 -0500

    Add Masa restaurant with 15 pizzas

---
+++ service-worker.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = "mancora-go-v28";
+
+// Delete ALL old caches on activate
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => caches.delete(cacheName))
+      );
+    }).then(() => {
+      return self.clients.claim();
+    })
+  );
+});
+
+self.addEventListener("install", event => {
+  self.skipWaiting();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const responseClone = response.clone();
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, responseClone);
+        });
+        return response;
+      })
+      .catch(() => {
+        return caches.match(event.request);
+      })
+  );
+});
